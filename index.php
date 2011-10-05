0b11456effd24e4c07580317218976318a3ac05d
Alex Yelizarov <felex-ae@ya.ru>
2011-10-05 16:44:14 +0400

    yeaah

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(E_ALL); 
+ini_set('display_errors',1);
 session_start();
+
 if(empty($_POST["username"]) or empty($_POST["password"])){
 	exit("Sorry, please fill all the fields.");
 } else {
